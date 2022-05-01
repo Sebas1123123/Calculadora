9494f07f618bce911b97bff80490e061c19d7a0d
Carlos Duran Vargas <carlosdurancr@gmail.com>
2022-04-30 20:21:26 -0600

    Actulizacion de la calculadora
    Está funcional

--- Project_Calc/Contenedor.java
+++ Project_Calc/Contenedor.java
@@ -3,7 +3,7 @@ import javax.swing.JComponent;
 
 /**
  * Clase contenedora para realizar pruebas de los métodos. Se tiene una interfaz
- * gráfica
+ * gráfica con un espacio para colocar una cifra y varias opciones, todas funcionales
  *
  * @author Sebastián
  * @author Sebastián España
@@ -17,24 +17,14 @@ public class Contenedor
     public static void main(String args[]) {
         RadioButtonCalc f = new RadioButtonCalc();
 
-        // Setting Bounds of JFrame.
+        // Tamaño de la pantalla de la calculadora.
         f.setBounds(200, 200, 800, 400);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Setting Title of frame.
+        // Título de la aplicación.
         f.setTitle("Calculadora de unidades de medida");
 
-        // Setting Visible status of frame as true.
+        // Visibilidad de la pantalla.
         f.setVisible(true);
-        /*Calculadora calc = new Calculadora();
-
-        System.out.println("3.5 metros a pies: " + calc.metroAPie(3.5));
-        System.out.println("0 metros a pies: " + calc.metroAPie(0));
-        System.out.println("11.4829 pies a metros es: " + calc.pieAMetro(11.4829));
-        System.out.println("7.8 centímetros a pulgadas es: " + calc.cmAPulgada(7.8));
-        System.out.println("3.07086 pulgadas a centimetros es: " + calc.pulgadaACm(3.0708678000000003));
-        System.out.println("120ºC a F es: " + calc.celAFar(120));
-        System.out.println("248ºF a C es: " + calc.farACel(248));
-        System.out.println("25 Km a millas es: " + calc.kmAMilla(25));
-        System.out.println("15.5 millas a Km es: " + calc.millaAKm(15.5343));*/
     }
 }
--- Project_Calc/RadioButtonCalc.java
+++ Project_Calc/RadioButtonCalc.java
@@ -1,18 +1,20 @@
 /**
  * Clase para crear botones de seleccion.
- * Adaptado de un ejemplo en el sitio https://docs.oracle.com/javase/tutorial/uiswing/examples/components/RadioButtonDemoProject/src/components/RadioButtonDemo.java
- * y de https://www.geeksforgeeks.org/jradiobutton-java-swing/
+ * Adaptado de un ejemplo en https://docs.oracle.com/javase/tutorial/uiswing/examples/components/RadioButtonDemoProject
+ * y de https://www.geeksforgeeks.org/jradiobutton-java-swing
  * 
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Sebastián España
+ * @author Sebastián Baltodano
+ * @author Carlos Durán
+ * @version 29-04-2022
  */
 
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-class RadioButtonCalc extends JFrame
+public class RadioButtonCalc extends JFrame
 {
     // Declaración de etiquetas para los botones
     static String metroAPie = "Metros a pies";
@@ -28,25 +30,25 @@ class RadioButtonCalc extends JFrame
     { //Constructor de la clase
         this.setLayout(null);
         
-        Calculadora calc = new Calculadora();
+        Calculadora calc = new Calculadora(); // instancia para los calculos de conversiones
         
-        //Creación de selectores
+        //Creación de selectores y coordenadas en la pantalla
         JRadioButton metroAPieButton = new JRadioButton(metroAPie);
-        metroAPieButton.setBounds(120, 50, 190, 50);
+        metroAPieButton.setBounds(120, 80, 190, 50);
         JRadioButton pieAMetroButton = new JRadioButton(pieAMetro);
-        pieAMetroButton.setBounds(120, 80, 190, 50);
+        pieAMetroButton.setBounds(120, 110, 190, 50);
         JRadioButton cmAPulgadaButton = new JRadioButton(cmAPulgada);
-        cmAPulgadaButton.setBounds(120, 110, 190, 50);
+        cmAPulgadaButton.setBounds(120, 140, 190, 50);
         JRadioButton pulgadaACmButton = new JRadioButton(pulgadaACm);
-        pulgadaACmButton.setBounds(120, 140, 190, 50);
+        pulgadaACmButton.setBounds(120, 170, 190, 50);
         JRadioButton celAFarButton = new JRadioButton(celAFar);
-        celAFarButton.setBounds(400, 50, 190, 50);
+        celAFarButton.setBounds(400, 80, 190, 50);
         JRadioButton farACelButton = new JRadioButton(farACel);
-        farACelButton.setBounds(400, 80, 190, 50);
+        farACelButton.setBounds(400, 110, 190, 50);
         JRadioButton kmAMillaButton = new JRadioButton(kmAMilla);
-        kmAMillaButton.setBounds(400, 110, 190, 50);
+        kmAMillaButton.setBounds(400, 140, 190, 50);
         JRadioButton millaAKmButton = new JRadioButton(millaAKm);
-        millaAKmButton.setBounds(400, 140, 190, 50);
+        millaAKmButton.setBounds(400, 170, 190, 50);
 
         //Group the radio buttons.
         ButtonGroup group = new ButtonGroup();
@@ -58,12 +60,18 @@ class RadioButtonCalc extends JFrame
         group.add(farACelButton);
         group.add(kmAMillaButton);
         group.add(millaAKmButton);
-
+        
+        JLabel lblEntrada = new JLabel("Ingrese el valor a convertir: ");
+        lblEntrada.setBounds(120, 10, 200, 50);
+        
+        JTextField entradaDato = new JTextField(10);
+        entradaDato.setBounds(290, 20, 100, 30);
+        
         JLabel etiqueta = new JLabel("Seleccione: ");
-        etiqueta.setBounds(310, 20, 150, 50);
+        etiqueta.setBounds(310, 50, 150, 50);
         
         JButton boton = new JButton("Calcular");
-        boton.setBounds(350, 210, 80, 30);
+        boton.setBounds(350, 230, 80, 30);
 
         //Agregar elementos a JFrame
         this.add(metroAPieButton);
@@ -76,52 +84,58 @@ class RadioButtonCalc extends JFrame
         this.add(millaAKmButton);
         this.add(etiqueta);
         this.add(boton);
+        this.add(lblEntrada);
+        this.add(entradaDato);
 
-        boton.addActionListener(new ActionListener() //Metodo para devolver un valor segun el boton seleccionado
+        boton.addActionListener(new ActionListener() 
             {
+                // Anonymous class.
                 public void actionPerformed(ActionEvent e)
                 {
+                    // Override Method
+                    // Declaration of String class Objects.
                     double resultado = 0.0;
 
+                    // If condition to check if jRadioButton2 is selected.
                     if (metroAPieButton.isSelected()) 
                     {
-                        resultado = calc.metroAPie(3.5);
+                        resultado = calc.metroAPie(Double.parseDouble(entradaDato.getText()));
                     }
 
                     else if (pieAMetroButton.isSelected()) 
                     {
-                        resultado = calc.pieAMetro(0);
+                        resultado = calc.pieAMetro(Double.parseDouble(entradaDato.getText()));
                     }
                     else if (cmAPulgadaButton.isSelected()) 
                     {
-                        resultado = calc.cmAPulgada(0);
+                        resultado = calc.cmAPulgada(Double.parseDouble(entradaDato.getText()));
                     }
                     else if (pulgadaACmButton.isSelected()) 
                     {
-                        resultado = calc.pulgadaACm(0);
+                        resultado = calc.pulgadaACm(Double.parseDouble(entradaDato.getText()));
                     }
                     else if (celAFarButton.isSelected()) 
                     {
-                        resultado = calc.celAFar(0);
+                        resultado = calc.celAFar(Double.parseDouble(entradaDato.getText()));
                     }
                     else if (farACelButton.isSelected()) 
                     {
-                        resultado = calc.farACel(0);
+                        resultado = calc.farACel(Double.parseDouble(entradaDato.getText()));
                     }
                     else if (kmAMillaButton.isSelected()) 
                     {
-                        resultado = calc.kmAMilla(0);
+                        resultado = calc.kmAMilla(Double.parseDouble(entradaDato.getText()));
                     }
                     else if (millaAKmButton.isSelected()) 
                     {
-                        resultado = calc.millaAKm(0);
+                        resultado = calc.millaAKm(Double.parseDouble(entradaDato.getText()));
                     }
                     else
                     {
                         resultado = 0.0;
                     }
 
-                    // Ventana con el mensaje del resultado.
+                    // MessageDialog para mostrar la informacion de la conversión.
                     JOptionPane.showMessageDialog(RadioButtonCalc.this, "El resultado es: " + resultado);
                 }
             });
